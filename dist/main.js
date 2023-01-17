248b8a7ec1f7f7b96b06218b2967f8f6cce234a6
kowp <90217593+nehkowp@users.noreply.github.com>
2023-01-17 10:32:45 +0100

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from './vendor/three/build/three.module.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
